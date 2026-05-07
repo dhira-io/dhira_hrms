356855c4653c3ecdd739005f1ea139ba02d4a09a
Saranya-Rajendiran <saranya.rajendiran@dhira.ai>
2026-05-07 13:10:20 +0530

    dashboard card size and icon changed to svg

--- lib/features/dashboard/presentation/bloc/bottom_nav_cubit.dart
+++ lib/features/dashboard/presentation/bloc/bottom_nav_cubit.dart
@@ -7,9 +7,8 @@ class BottomNavCubit extends Cubit<int> {
   static const int homeIndex = 0;
   static const int attendanceIndex = 1;
   static const int approvalsIndex = 2;
-  static const int myOrgIndex = 3;
-  static const int notificationsIndex = 4;
-  static const int settingsIndex = 5;
+  // static const int myOrgIndex = 3;
+  static const int settingsIndex = 3;
 
   void changeIndex(int index) => emit(index);
 }
